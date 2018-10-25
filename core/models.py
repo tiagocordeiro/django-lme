07a9120635396a139f5394d25fd56cb25b4e924d
Tiago Cordeiro <tiago@mulhergorila.com>
2018-10-25 01:19:42 -0300

    Configura Model para LME Salva Pandas Data Frame no banco

--- core/models.py
+++ core/models.py
@@ -3,4 +3,21 @@ from django.db import models
 
 class TimeSerie(models.Model):
     name = models.CharField(max_length=255, blank=False, null=False)
-    code = models.CharField(max_length=50, blank=False, null=False)
+    code = models.CharField(unique=True, max_length=50, blank=False, null=False)
+
+
+class Serie(models.Model):
+    code = models.ForeignKey(TimeSerie, on_delete=models.CASCADE)
+    date = models.DateField()
+    cash = models.DecimalField(decimal_places=2, max_digits=20)
+
+class LondonMetalExchange(models.Model):
+    date = models.DateField(unique=True)
+    cobre = models.DecimalField(decimal_places=2, max_digits=20)
+    zinco = models.DecimalField(decimal_places=2, max_digits=20)
+    aluminio = models.DecimalField(decimal_places=2, max_digits=20)
+    chumbo = models.DecimalField(decimal_places=2, max_digits=20)
+    estanho = models.DecimalField(decimal_places=2, max_digits=20)
+    niquel = models.DecimalField(decimal_places=2, max_digits=20)
+    dolar = models.DecimalField(decimal_places=2, max_digits=20)
+
--- core/views.py
+++ core/views.py
@@ -1,8 +1,10 @@
 import quandl
+from decouple import config
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from sqlalchemy import create_engine
 
-from .models import TimeSerie
+from .models import TimeSerie, LondonMetalExchange
 
 
 def index(request):
@@ -11,19 +13,34 @@ def index(request):
 
 @login_required
 def update_database(request):
+    lme = LondonMetalExchange.objects.all()
     timeseries = TimeSerie.objects.all()
     lista = []
+    colunas = []
     for serie in timeseries:
         lista.append(serie.code)
+        colunas.append(serie.name)
 
     todo_periodo = quandl.get(lista,
                               start_date='2012-01-03',
                               returns='pandas')
 
+    todo_periodo.columns = colunas
+
+    engine = create_engine(config('DATABASE_URL'))
+
+    connection = engine.connect()
+
+    todo_periodo.to_sql('core_londonmetalexchange', connection, if_exists='replace')
+
+    connection.close()
+
     context = {
         'timeseries': timeseries,
         'lista': lista,
         'todo_periodo': todo_periodo,
+        'colunas': colunas,
+        'lme': lme,
     }
 
     return render(request, 'update_database.html', context)
