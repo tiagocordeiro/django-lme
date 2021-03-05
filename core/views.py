573c3d91e84f265277638555d26001c247d13598
Tiago Cordeiro <tiago@mulhergorila.com>
2021-03-05 09:23:39 -0300

    Refatora
    - Refatora views e ajusta alguns templates
    - Adiciona fachada para aplicação `core`
    - Melhora urls
    - atualiza quandl para 3.6.1

--- core/views.py
+++ core/views.py
@@ -1,13 +1,15 @@
 import datetime
-from datetime import datetime, timedelta
+from datetime import datetime
 
 import pandas as pd
 import quandl
 from decouple import config
 from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
 from django.shortcuts import render
 from sqlalchemy import create_engine
 
+from .facade import get_lme, get_last_five_weeks, get_last, json_builder
 from .models import TimeSerie, LondonMetalExchange
 
 
@@ -20,16 +22,25 @@ def index(request):
     return render(request, 'index.html', context)
 
 
-def chart(request, chart_id='chart_ID', chart_type='line', chart_height=350):
-    lme = LondonMetalExchange.objects.all().order_by('-date')
+def api_view(request, date_from=get_last_five_weeks(), date_to=get_last(), limit=100):
+    lme_prices = get_lme(date_from=date_from, date_to=date_to, limit=limit)
+
+    json_data = json_builder(lme_prices)
+
+    data = {"results": json_data}
+    return JsonResponse(data)
+
+
+def chart(request, date_from=get_last_five_weeks(), date_to=get_last(),
+          chart_id='chart_ID', chart_type='line', chart_height=350):
+    date_to = datetime.strptime(date_to, '%d-%m-%Y')
+    date_from = datetime.strptime(date_from, '%d-%m-%Y')
+
+    lme = LondonMetalExchange.objects.all().order_by('-date').filter(date__range=(date_from, date_to))
 
     lme_last = LondonMetalExchange.objects.values('date').last()
 
-    date_to = lme_last['date']
-    date_from = date_to - timedelta(weeks=5)
-
-    lme_periodo = LondonMetalExchange.objects.filter(
-        date__range=(date_from, date_to))
+    lme_periodo = LondonMetalExchange.objects.filter(date__range=(date_from, date_to))
 
     df = pd.DataFrame(
         list(lme_periodo.values('date', 'cobre', 'zinco', 'aluminio',
@@ -70,6 +81,7 @@ def chart(request, chart_id='chart_ID', chart_type='line', chart_height=350):
     yAxis = {"title": {"text": 'Valor'}}
 
     context = {
+
         'lme': lme,
         'xAxis': xAxis,
         'yAxis': yAxis,
@@ -97,63 +109,6 @@ def periodo(request, date_from, date_to):
         'date_to': date_to,
     }
     return render(request, 'index.html', context)
-
-
-def chart_periodo(request, date_from, date_to, chart_id='chart',
-                  chart_type='line', chart_height=350):
-    date_from = datetime.strptime(date_from, '%d-%m-%Y')
-    date_to = datetime.strptime(date_to, '%d-%m-%Y')
-    lme = LondonMetalExchange.objects.filter(date__range=(date_from, date_to))
-
-    df = pd.DataFrame(list(lme.values('date', 'cobre', 'zinco', 'aluminio',
-                                      'chumbo', 'estanho', 'niquel',
-                                      'dolar', )))
-
-    df['date'] = pd.to_datetime(df['date'], utc=True)
-
-    df = df.set_index(df['date'])
-
-    df = df.drop('date', axis=1)
-
-    df.fillna(method='ffill', inplace=True)
-    df.fillna(method='bfill', inplace=True)
-
-    cobre = [float(item) for item in list(df['cobre'])]
-    zinco = [float(item) for item in list(df['zinco'])]
-    aluminio = [float(item) for item in list(df['aluminio'])]
-    chumbo = [float(item) for item in list(df['chumbo'])]
-    estanho = [float(item) for item in list(df['estanho'])]
-    niquel = [float(item) for item in list(df['niquel'])]
-    dolar = [float(item) for item in list(df['dolar'])]
-    data = list(df.index.strftime('%d/%m/%y'))
-
-    chart = {"renderTo": chart_id, "type": chart_type,
-             "height": chart_height}
-
-    series = [{"name": 'Cobre', "data": cobre},
-              {"name": 'Zinco', "data": zinco},
-              {"name": 'Alumínio', "data": aluminio},
-              {"name": 'Chumbo', "data": chumbo},
-              {"name": 'Estanho', "data": estanho},
-              {"name": 'Níquel', "data": niquel},
-              {"name": 'Dolar', "data": dolar}
-              ]
-
-    title = {"text": 'Cotação LME'}
-    xAxis = {"categories": data, "crosshair": 'true'}
-    yAxis = {"title": {"text": 'Valor'}}
-
-    context = {
-        'lme': lme,
-        'xAxis': xAxis,
-        'yAxis': yAxis,
-        'series': series,
-        'title': title,
-        'chart': chart,
-        'chart_id': chart_id,
-    }
-
-    return render(request, 'chart.html', context)
 
 
 @login_required
